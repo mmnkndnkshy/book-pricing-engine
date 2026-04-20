5931085cbf05124a7463ba948156fedb1a03d711
Manikandan Muthuchamy <manikandanmuthuchamy@gmail.com>
2026-04-21 00:57:30 +0530

    test: align pricing tests and swagger contract with map-based request model

--- src/test/java/com/mmnkndn/kata/bookpricing/service/PricingServiceTest.java
+++ src/test/java/com/mmnkndn/kata/bookpricing/service/PricingServiceTest.java
@@ -1,12 +1,16 @@
 package com.mmnkndn.kata.bookpricing.service;
 
-import com.mmnkndn.kata.bookpricing.domain.Basket;
-import com.mmnkndn.kata.bookpricing.domain.Book;
+
+import com.mmnkndn.kata.bookpricing.api.model.Book;
+import com.mmnkndn.kata.bookpricing.api.model.BookPricingRequest;
+
+import com.mmnkndn.kata.bookpricing.api.model.BookPricingResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -16,105 +20,108 @@ class PricingServiceTest {
     @Autowired
     private PricingService pricingService;
 
+    private BookPricingRequest buildRequest(Map<String, Integer> items) {
+
+        BookPricingRequest request = new BookPricingRequest();
+        request.setItems(items);
+
+        return request;
+    }
+
+
     @Test
     void shouldReturnZeroWhenBasketIsEmpty() {
-        Basket basket = new Basket(List.of());
+        BookPricingRequest request = buildRequest(Map.of());
 
-        double price = pricingService.calculatePrice(basket);
+        BookPricingResponse response = pricingService.calculatePrice(request);
 
-        assertEquals(0.0, price);
+        assertEquals(0.0, response.getPricing().getFinalPrice());
     }
 
     @Test
     void shouldReturnFiftyForSingleBook() {
-        Basket basket = new Basket(List.of(Book.CLEAN_CODE));
+        BookPricingRequest request = buildRequest(Map.of("CLEAN_CODE", 1));
 
-        double price = pricingService.calculatePrice(basket);
+        BookPricingResponse response = pricingService.calculatePrice(request);
 
-        assertEquals(50.0, price);
+        assertEquals(50.0, response.getPricing().getFinalPrice());
     }
 
     @Test
     void shouldReturnHundredForTwoSameBooks() {
-        Basket basket = new Basket(List.of(
-                Book.CLEAN_CODE,
-                Book.CLEAN_CODE
-        ));
+        BookPricingRequest request = buildRequest(Map.of("CLEAN_CODE", 2));
 
-        double price = pricingService.calculatePrice(basket);
+        BookPricingResponse response = pricingService.calculatePrice(request);
 
-        assertEquals(100.0, price);
+        assertEquals(100.0, response.getPricing().getFinalPrice());
     }
 
     @Test
     void shouldApplyFivePercentDiscountForTwoDifferentBooks() {
-        Basket basket = new Basket(List.of(
-                Book.CLEAN_CODE,
-                Book.CLEAN_CODER
+        BookPricingRequest request = buildRequest(Map.of(
+                "CLEAN_CODE", 1,
+                "CLEAN_CODER", 1
         ));
 
-        double price = pricingService.calculatePrice(basket);
+        BookPricingResponse response = pricingService.calculatePrice(request);
 
-        assertEquals(95.0, price);
+        assertEquals(95.0, response.getPricing().getFinalPrice());
     }
 
     @Test
     void shouldApplyTenPercentDiscountForThreeDifferentBooks() {
-        Basket basket = new Basket(List.of(
-                Book.CLEAN_CODE,
-                Book.CLEAN_CODER,
-                Book.CLEAN_ARCHITECTURE
+        BookPricingRequest request = buildRequest(Map.of(
+                "CLEAN_CODE", 1,
+                "CLEAN_CODER", 1,
+                "CLEAN_ARCHITECTURE", 1
         ));
 
-        double price = pricingService.calculatePrice(basket);
+        BookPricingResponse response = pricingService.calculatePrice(request);
 
-        assertEquals(135.0, price);
+        assertEquals(135.0, response.getPricing().getFinalPrice());
     }
 
     @Test
     void shouldApplyTwentyPercentDiscountForFourDifferentBooks() {
-        Basket basket = new Basket(List.of(
-                Book.CLEAN_CODE,
-                Book.CLEAN_CODER,
-                Book.CLEAN_ARCHITECTURE,
-                Book.TDD_BY_EXAMPLE
+        BookPricingRequest request = buildRequest(Map.of(
+                "CLEAN_CODE", 1,
+                "CLEAN_CODER", 1,
+                "CLEAN_ARCHITECTURE", 1,
+                "TDD_BY_EXAMPLE",1
         ));
 
-        double price = pricingService.calculatePrice(basket);
+        BookPricingResponse response = pricingService.calculatePrice(request);
 
-        assertEquals(160.0, price);
+        assertEquals(160.0, response.getPricing().getFinalPrice());
     }
 
     @Test
     void shouldApplyTwentyFivePercentDiscountForFiveDifferentBooks() {
-        Basket basket = new Basket(List.of(
-                Book.CLEAN_CODE,
-                Book.CLEAN_CODER,
-                Book.CLEAN_ARCHITECTURE,
-                Book.TDD_BY_EXAMPLE,
-                Book.LEGACY_CODE
+        BookPricingRequest request = buildRequest(Map.of(
+                "CLEAN_CODE", 1,
+                "CLEAN_CODER", 1,
+                "CLEAN_ARCHITECTURE", 1,
+                "TDD_BY_EXAMPLE",1,
+                "LEGACY_CODE",1
         ));
 
-        double price = pricingService.calculatePrice(basket);
+        BookPricingResponse response = pricingService.calculatePrice(request);
 
-        assertEquals(187.5, price);
+        assertEquals(187.5, response.getPricing().getFinalPrice());
     }
 
     @Test
     void shouldCalculateOptimalPriceForComplexBasket() {
-        Basket basket = new Basket(List.of(
-                Book.CLEAN_CODE,
-                Book.CLEAN_CODE,
-                Book.CLEAN_CODER,
-                Book.CLEAN_CODER,
-                Book.CLEAN_ARCHITECTURE,
-                Book.CLEAN_ARCHITECTURE,
-                Book.TDD_BY_EXAMPLE,
-                Book.LEGACY_CODE
+        BookPricingRequest request = buildRequest(Map.of(
+                "CLEAN_CODE", 2,
+                "CLEAN_CODER", 2,
+                "CLEAN_ARCHITECTURE", 2,
+                "TDD_BY_EXAMPLE",1,
+                "LEGACY_CODE",1
         ));
 
-        double price = pricingService.calculatePrice(basket);
+        BookPricingResponse response = pricingService.calculatePrice(request);
 
-        assertEquals(320.0, price);
+        assertEquals(320.0, response.getPricing().getFinalPrice());
     }
 }
