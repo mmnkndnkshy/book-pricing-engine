c4afc5c6b0836c936ba22daf5ae24b4cd863d3a3
Manikandan Muthuchamy <manikandanmuthuchamy@gmail.com>
2026-04-20 20:27:07 +0530

    refactor: make GreedyGroupingStrategy a Spring component and avoid mutating input map

--- src/main/java/com/mmnkndn/kata/bookpricing/grouping/GreedyGroupingStrategy.java
+++ src/main/java/com/mmnkndn/kata/bookpricing/grouping/GreedyGroupingStrategy.java
@@ -1,28 +1,40 @@
 package com.mmnkndn.kata.bookpricing.grouping;
 
 import com.mmnkndn.kata.bookpricing.domain.Book;
+import org.springframework.stereotype.Component;
 
 import java.util.*;
 
-
+@Component
 public class GreedyGroupingStrategy implements GroupingStrategy {
+
     @Override
     public List<Integer> group(Map<Book, Integer> counts) {
+
+        Map<Book, Integer> remaining = new HashMap<>(counts);
+
         List<Integer> groupSizes = new ArrayList<>();
-        while (!counts.isEmpty()) {
+
+        while (!remaining.isEmpty()) {
             Set<Book> group = new HashSet<>();
-            Iterator<Map.Entry<Book, Integer>> it = counts.entrySet().iterator();
+
+            Iterator<Map.Entry<Book, Integer>> it = remaining.entrySet().iterator();
+
             while (it.hasNext()) {
                 Map.Entry<Book, Integer> entry = it.next();
+
                 group.add(entry.getKey());
+
                 if (entry.getValue() == 1) {
                     it.remove();
                 } else {
                     entry.setValue(entry.getValue() - 1);
                 }
             }
+
             groupSizes.add(group.size());
         }
+
         return groupSizes;
     }
 }
