6aec0a25f0c28a2fae88a4b60e6a3e446d442f84
Manikandan Muthuchamy <manikandanmuthuchamy@gmail.com>
2026-04-20 17:01:45 +0530

    refactor: update PricingService to delegate pricing via PricingStrategy

--- src/main/java/com/mmnkndn/kata/bookpricing/service/PricingService.java
+++ src/main/java/com/mmnkndn/kata/bookpricing/service/PricingService.java
@@ -1,103 +1,21 @@
 package com.mmnkndn.kata.bookpricing.service;
 
 import com.mmnkndn.kata.bookpricing.domain.Basket;
-import com.mmnkndn.kata.bookpricing.domain.Book;
-import com.mmnkndn.kata.bookpricing.grouping.GreedyGroupingStrategy;
-import com.mmnkndn.kata.bookpricing.grouping.GroupingStrategy;
-import com.mmnkndn.kata.bookpricing.pricing.DefaultDiscountPolicy;
-import com.mmnkndn.kata.bookpricing.pricing.DiscountPolicy;
-
-import java.util.*;
+import com.mmnkndn.kata.bookpricing.pricing.*;
 
 public class PricingService {
-    private final DiscountPolicy discountPolicy = new DefaultDiscountPolicy();
 
-    private final GroupingStrategy groupingStrategy = new GreedyGroupingStrategy();
+    private final PricingStrategy pricingStrategy;
 
-    private static final double BOOK_PRICE = 50.0;
+    public PricingService() {
+        this.pricingStrategy = new GreedyPricingStrategy(); // default
+    }
 
-    private static final Map<Integer, Double> DISCOUNTS = new HashMap<>();
-
-    static {
-        DISCOUNTS.put(1, 0.0);
-        DISCOUNTS.put(2, 0.05);
-        DISCOUNTS.put(3, 0.10);
-        DISCOUNTS.put(4, 0.20);
-        DISCOUNTS.put(5, 0.25);
+    public PricingService(PricingStrategy strategy) {
+        this.pricingStrategy = strategy;
     }
 
     public double calculatePrice(Basket basket) {
-        if (basket == null || basket.getBooks().isEmpty()) return 0.0;
-
-        Map<Book, Integer> counts = countBooks(basket.getBooks());
-        List<Integer> groupSizes = groupingStrategy.group(counts);
-        optimizeGroups(groupSizes);
-
-        return calculateTotal(groupSizes);
-    }
-
-
-    private Map<Book, Integer> countBooks(List<Book> basket) {
-        Map<Book, Integer> counts = new HashMap<>();
-
-        for (Book book : basket) {
-            counts.put(book, counts.getOrDefault(book, 0) + 1);
-        }
-
-        return counts;
-    }
-
-    private List<Integer> buildGroups(Map<Book, Integer> counts) {
-        List<Integer> groupSizes = new ArrayList<>();
-
-        while (!counts.isEmpty()) {
-            Set<Book> group = new HashSet<>();
-
-            Iterator<Map.Entry<Book, Integer>> it = counts.entrySet().iterator();
-
-            while (it.hasNext()) {
-                Map.Entry<Book, Integer> entry = it.next();
-
-                group.add(entry.getKey());
-
-                if (entry.getValue() == 1) {
-                    it.remove();
-                } else {
-                    entry.setValue(entry.getValue() - 1);
-                }
-            }
-
-            groupSizes.add(group.size());
-        }
-
-        return groupSizes;
-    }
-
-    private void optimizeGroups(List<Integer> groupSizes) {
-        while (groupSizes.contains(5) && groupSizes.contains(3)) {
-            groupSizes.remove(Integer.valueOf(5));
-            groupSizes.remove(Integer.valueOf(3));
-            groupSizes.add(4);
-            groupSizes.add(4);
-        }
-    }
-
-    private double calculateTotal(List<Integer> groupSizes) {
-
-        Map<Integer, Double> discounts = new HashMap<>();
-        discounts.put(1, 0.0);
-        discounts.put(2, 0.05);
-        discounts.put(3, 0.10);
-        discounts.put(4, 0.20);
-        discounts.put(5, 0.25);
-
-        double total = 0.0;
-
-        for (int size : groupSizes) {
-            double discount = discountPolicy.getDiscount(size);
-            total += size * 50.0 * (1 - discount);
-        }
-
-        return total;
+        return pricingStrategy.calculatePrice(basket);
     }
 }
