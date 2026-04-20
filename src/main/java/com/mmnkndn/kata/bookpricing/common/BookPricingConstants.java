4dd236a180c89b032bdd323dbd7726930a276d62
Manikandan Muthuchamy <manikandanmuthuchamy@gmail.com>
2026-04-21 01:51:39 +0530

    refactor: introduce common constants for pricing configuration

---
+++ src/main/java/com/mmnkndn/kata/bookpricing/common/BookPricingConstants.java
@@ -0,0 +1,14 @@
+package com.mmnkndn.kata.bookpricing.common;
+
+public final class BookPricingConstants {
+
+    private BookPricingConstants() {
+        // prevent instantiation
+    }
+
+    public static final double BOOK_PRICE = 50.0;
+
+    public static final int MAX_BOOK_GROUP_SIZE = 5;
+
+    public static final double NO_DISCOUNT = 0.0;
+}
